d58f59f5a3db416f90cef6d8e96910738402b2e3
Roland Kiss <2021067@dszcberegszaszi.hu>
2024-10-02 08:13:59 +0200

    enum

---
+++ macskak/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace macskak
+{
+    class Program
+    {
+        enum Animals
+        {
+            Macska,Kutya,Farkas,Tigris
+        }
+        static void Main(string[] args)
+        {
+            Animals b = Animals.Farkas;
+            Animals c = Animals.Macska;
+            if(b ==Animals.Farkas)
+            {
+                Console.WriteLine("A b állat farkas");
+            }
+            if (c == Animals.Macska)
+            {
+                Console.WriteLine("A c állat Macska");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
