bf2dc731838967f5bb881a9b0008c68ecde72499
Roland Kiss <2021067@dszcberegszaszi.hu>
2024-10-02 09:54:47 +0200

    if

---
+++ if/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = true;
+            if (b) Console.WriteLine("Ez az igaz ág");
+            else Console.WriteLine("Ez a hamis ág");
+
+
+            b = false;
+            if (b) Console.WriteLine("Ez az igaz ág");
+            else Console.WriteLine("Ez a hamis ág");
+
+
+        }
+    }
+}
