8fd29122ef43eb1d942e07b4e9b9322c10d15fb3
Roland Kiss <2021067@dszcberegszaszi.hu>
2024-10-09 08:52:06 +0200

    korhasab

---
+++ korhasab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace korhasab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1. objektum
+            kor k1 = new kor();
+            k1.readSugar(15.0);
+            k1.SetKerulet();
+            k1.SetTerulet();
+            Console.WriteLine($"{k1.GetSugar()}sugarú kör kerülete: {k1.GetKerulet()},Területe: {k1.GetTerulet()}") ;
+            #endregion EOf 1. objektum
+            #region 2.objektum
+            kor k2 = new kor(28.7);
+            k2.SetKerulet();
+            k2.SetTerulet();
+            Console.WriteLine($"{k2.GetSugar()}sugarú kör kerülete: {k2.GetKerulet()},Területe: {k2.GetTerulet()}");
+            #endregion Eof 2. objektum
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ korhasab/kor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace korhasab
+{
+    class kor
+    {
+        //Az osztály feladata a sugárból kiszámitja a kör területét és kerületét
+
+        //Osztályváltozók
+
+        //Konstruktoro
+        private double sugar,
+        kerulet,
+        terulet;
+
+
+        public kor() { }
+        public kor(double r) {
+
+            this.sugar = r;
+        }
+        public void readSugar(double r)
+        {
+            this.sugar = r;
+        }
+
+        public void SetKerulet()
+        {
+            this.kerulet = 2 * this.sugar * Math.PI;
+        }
+        public void SetTerulet()
+        {
+
+            double eredmeny1,eredmeny2 = 0.0;
+            
+           eredmeny1 = this.terulet = this.sugar  * this.sugar * Math.PI;
+
+            eredmeny2 = Math.Pow(this.sugar, 2) * Math.PI;
+            if(eredmeny1 == eredmeny2)
+            {
+                this.terulet = eredmeny2;
+            }
+            else
+            {
+                Console.WriteLine($"Eredmény: {eredmeny1}!= Eredmény2 {eredmeny2}");
+            }
+        }
+        public double GetKerulet() { return this.kerulet; }
+
+        public double GetTerulet() { return this.terulet; }
+
+        public double GetSugar() { return this.sugar; }
+    }
+}
