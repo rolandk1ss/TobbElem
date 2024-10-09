901f07ce6670e34ebe36d1bf3dfd85924e31dbb1
Roland Kiss <2021067@dszcberegszaszi.hu>
2024-10-09 10:04:10 +0200

    tegla

---
+++ tegla/program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tegla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teglalapszamitasok teglaszam = new Teglalapszamitasok();
+            teglaszam.ReadAoldal(15);
+            teglaszam.ReadBoldal(25);
+            teglaszam.TeruletSzam();
+            teglaszam.KeruletSzam();
+            Console.WriteLine($"A kerület az : {teglaszam.GetKerulet()} A terület: {teglaszam.GetTerulet()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tegla/teglalap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tegla
+{
+    class Teglalapszamitasok
+    {
+        private double aoldal,
+            boldal,
+            terulet,
+            kerulet;
+
+        public Teglalapszamitasok()
+        {
+
+        }
+        public Teglalapszamitasok(double a, double b)
+        {
+            this.aoldal = a;
+            this.boldal = b;
+        }
+       
+        public void ReadAoldal(double aoldal)
+        {
+            this.aoldal = aoldal;
+        }
+        public void ReadBoldal(double boldal)
+        {
+            this.boldal = boldal;
+        }
+        
+        public void TeruletSzam()
+        {
+            this.terulet = this.aoldal * this.boldal;
+        }
+        public void KeruletSzam()
+        {
+            this.kerulet = (this.aoldal + this.boldal) * 2;
+        }
+        public double GetKerulet() { return kerulet; }
+        public double GetTerulet() { return terulet; }
+
+
+    }
+
+}
