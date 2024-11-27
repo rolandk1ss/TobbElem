bd47e4cb1d16dc061e862b4eb2c2b6c7604fe1b1
Roland Kiss <2021067@dszcberegszaszi.hu>
2024-11-27 10:07:20 +0100

    asd

---
+++ Kivetel/Kivetel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetel
+{
+    class Kivetel
+    {
+        public void KivKezNincs()
+        {
+            Console.WriteLine("Nincs kivetelkezeles");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("A beírt szám: {0}",szam);
+
+           
+        }
+        public void KivKezAlt()
+        {
+            Console.WriteLine("Hiba");
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(("A beírt szám: {0}");
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString();
+            }
+            Console.ReadKey();
+        }
+    }
+}
