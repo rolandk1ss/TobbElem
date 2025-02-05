55a8947d68f019c190c1c91e612a163f923017cd
Roland Kiss <2021067@dszcberegszaszi.hu>
2025-02-05 10:07:47 +0100

    asd

---
+++ PoliformOOP/PoliformOOPProg.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliformOOP
+{
+    class PoliformOOPProg
+    {
+        static void Main(string[] args)
+        {
+            Haz otthon = new Haz();
+
+            Woman eva = new Woman("Éva", "zöld");
+            Woman kati = new Woman("Kati", "barna");
+            Woman sari = new Woman("Sári", "kék");
+            Woman zsuzsi = new Woman("Zsuzsi", "zöld");
+
+            Ferfi adam = new Ferfi("Ádám", 26);
+            Ferfi dani = new Ferfi("Dani", 26);
+            Ferfi gergo = new Ferfi("Gergő", 26);
+            Ferfi atti = new Ferfi("Attila", 26);
+
+            if (eva is Ember) otthon.hazajon
+            (eva as Ember);
+            otthon.hazajon((Ember)kati);
+            otthon.hazajon((Ember)sari);
+            otthon.hazajon((Ember)zsuzsi);
+            otthon.hazajon((Ember)adam);
+            otthon.hazajon((Ember)dani);
+            otthon.hazajon((Ember)gergo);
+            otthon.hazajon((Ember)atti);
+        }
+    }
+    class Ember
+    {
+        public string Nev { set; get; }
+
+        private string bemut = "Üdvözlöm, a nevem: ";
+
+        public Ember(string nev)
+        {
+            Nev = nev;
+        }
+
+        public string beszel()
+        {
+            return (bemut + Nev);
+        }
+    }
+
+    class Ferfi : Ember
+    {
+        public int Kor { set; get; }
+        public Ferfi(string nev, int kor) : base(nev)
+        {
+            Kor = kor;
+        }
+    }
+
+    class Woman : Ember
+    {
+        public string SzemSzin { set; get; }
+
+        public Woman(string nev, string szemszin) : base(nev)
+        {
+            SzemSzin = szemszin;
+        }
+    }
+
+    class Haz
+    {
+        private List<Ember> lakok = new List<Ember>();
+
+        public void hazajon(Ember obj)
+        {
+            this.lakok.Add(obj);
+        }
+
+        public void csenget()
+        {
+            int i;
+            Random rd = new Random();
+            if (lakok.Count > 0)
+            {
+                i = rd.Next(0, lakok.Count - 1);
+                Console.WriteLine(lakok[i].beszel());
+            }
+        }
+    }
+}
---
+++ gettersetter/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gettersetter
+{
+    class settergetter
+    {
+        static void Main(string[] args)
+        {
+            person person = new person();
+            person.Name = "Ádám";
+            Console.WriteLine(person.Name);
+
+            person.Id = 99;
+            Console.WriteLine(person.Id);
+
+            Console.ReadKey();
+        }
+    }
+    class person
+    {
+        public person() { }
+
+        public string Name { get; set; }
+
+        private int id;
+        public int Id { get => id; set => id = 101; }
+
+        private int age;
+        public void setAge(int age) { this.age = age; }
+
+        public int getAge(int age) { return this.age; }
+
+        private int gender;
+
+        public void setGender(string gender)
+        {
+            switch (gender)
+            {
+                case "Férfi":
+                    this.gender = 1;
+                    break;
+                case "Nő":
+                    this.gender = 0;
+                    break;
+                default:
+                    this.gender = 3;
+                    break;
+            }
+        }
+        public string getGender()
+        {
+            switch (this.gender)
+            {
+                case 1: return "Férfi";
+                case 2: return "Nő";
+                default: return "Ismeretlen";
+            }
+        }
+    }
+}
